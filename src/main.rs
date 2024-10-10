7bf12da6edd6035395e9eead53166f79015f3373
Ye Xia <only.one.truth.1102@gmail.com>
2024-10-10 11:40:53 +0800

    add sysinfo

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@ use anyhow::Result;
 use futures::prelude::*;
 use log::{debug, info};
 use redis::Client;
+use sysinfo::System;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::init();
+    let mut sys = System::new_all();
+    sys.refresh_all();
 
     let client = Client::open("rediss://localhost:6700".to_string())?;
     let pchannels = vec!["test*".to_string()];
@@ -19,11 +22,22 @@ async fn main() -> Result<()> {
     let mut sub_stream = sub_conn.on_message();
 
     loop {
+        let start = Instant::now();
         let next_msg = sub_stream.next().await;
         match next_msg {
             Some(msg) => {
-                info!("got message with size: {}", msg.get_payload_bytes().len());
-                debug!("new event: {}", msg.get_channel_name());
+                sys.refresh_memory();
+                sys.refresh_cpu_usage();
+                info!(
+                    "got message on channel: {}, size: {}KB, time: {:?} cpu: {} mem: {}MB",
+                    msg.get_channel_name(),
+                    msg.get_payload_bytes().len() / 1024,
+                    start.elapsed(),
+                    sys.global_cpu_usage(),
+                    sys.used_memory() / 1024 / 1024,
+                );
+                // info!("got message with size: {}", msg.get_payload_bytes().len());
+                // debug!("new event: {}", msg.get_channel_name());
             }
             None => {
                 info!("Next returns NONE, somehow stream is disconnected");
@@ -32,10 +46,11 @@ async fn main() -> Result<()> {
         }
     }
 }
-
  
  
 
         
 
         
+use std::time::Instant;
+
